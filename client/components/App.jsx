c9a28ef9272ddeb1ed5e0244d6c14128faae6542
anthony-xiao <juniorxiao01@gmail.com>
2018-06-21 11:41:44 +1200

    started app jsx and adding in addRest fn

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
 class App extends React.Component {
-  
-}
+  constructor (props) {
+    super(props)
+    this.state = {
+      newRestaurant: '',
+      currentRestaurants: ['Depot', 'Masu']
+    }
+    this.addRestaurant = this.addRestaurant.bind(this)
+  }
 
+  addRestaurant () {
+    this.setState({
+      
+    })
+  }
 
-
-
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+  render () {
+    return (
+      <Router>
+        <div>
+          <h2>Restaurant List</h2>
+          <p>{this.state.currentRestaurants}</p>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App
